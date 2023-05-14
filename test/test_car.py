edd9f6190d67832e59c90dac13090c8f7ede6438
tejasviarora23 <133563625+tejasviarora23@users.noreply.github.com>
2023-05-14 13:35:40 +0530

    Update test_car.py

--- test/test_car.py
+++ test/test_car.py
@@ -1,70 +1,21 @@
-import unittest
-from datetime import datetime
-from engine.model.calliope import Calliope
-from engine.model.glissade import Glissade
-from engine.model.palindrome import Palindrome
-from engine.model.rorschach import Rorschach
-from engine.model.thovex import Thovex
+#unit test for the Car base class:
 
+class TestCar(unittest.TestCase):
+    def test_needs_service_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            car = Car(datetime.today().date())
+            car.needs_service()
 
-def test_car(car_class, last_service_years_ago, current_mileage, last_service_mileage, warning_light_is_on):
-    today = datetime.today().date()
-    last_service_date = today.replace(year=today.year - last_service_years_ago)
-    car = car_class(last_service_date, current_mileage, last_service_mileage, warning_light_is_on)
-    return car.needs_service()
-
-
-class TestCalliope(unittest.TestCase):
+#unit test for the Thovex class:
+class TestThovex(unittest.TestCase):
     def test_battery_should_be_serviced(self):
-        self.assertTrue(test_car(Calliope, 3, 0, 0, False))
+        self.assertTrue(Thovex(datetime(2020, 1, 1), 0, 0, False).needs_service())
 
     def test_battery_should_not_be_serviced(self):
-        self.assertFalse(test_car(Calliope, 1, 0, 0, False))
+        self.assertFalse(Thovex(datetime(2022, 1, 1), 0, 0, False).needs_service())
 
     def test_engine_should_be_serviced(self):
-        self.assertTrue(test_car(Calliope, 0, 30001, 0, False))
+        self.assertTrue(Thovex(datetime(2020, 1, 1), 60001, 0, False).needs_service())
 
     def test_engine_should_not_be_serviced(self):
-        self.assertFalse(test_car(Calliope, 0, 30000, 0, False))
-
-
-class TestGlissade(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        self.assertTrue(test_car(Glissade, 3, 0, 0, False))
-
-    def test_battery_should_not_be_serviced(self):
-        self.assertFalse(test_car(Glissade, 1, 0, 0, False))
-
-    def test_engine_should_be_serviced(self):
-        self.assertTrue(test_car(Glissade, 0, 60001, 0, False))
-
-    def test_engine_should_not_be_serviced(self):
-        self.assertFalse(test_car(Glissade, 0, 60000, 0, False))
-
-
-class TestPalindrome(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        self.assertTrue(test_car(Palindrome, 5, 0, 0, False))
-
-    def test_battery_should_not_be_serviced(self):
-        self.assertFalse(test_car(Palindrome, 3, 0, 0, False))
-
-    def test_engine_should_be_serviced(self):
-        self.assertTrue(test_car(Palindrome, 0, 0, 0, True))
-
-    def test_engine_should_not_be_serviced(self):
-        self.assertFalse(test_car(Palindrome, 0, 0, 0, False))
-
-
-class TestRorschach(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        self.assertTrue(test_car(Rorschach, 5, 0, 0, False))
-
-    def test_battery_should_not_be_serviced(self):
-        self.assertFalse(test_car(Rorschach, 3, 0, 0, False))
-
-    def test_engine_should_be_serviced(self):
-        self.assertTrue(test_car(Rorschach, 0, 60001, 0, False))
-
-    def test_engine_should_not_be_serviced(self):
-        self.assertFalse(test_car(Rorschach, 0, 60000, 0, False))
+        self.assertFalse(Thovex(datetime(2020, 1, 1), 60000, 0, False).needs_service())
