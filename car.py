bbeac28e6077b8bd749b74875ea3f80bb95cda4c
tejasviarora23 <133563625+tejasviarora23@users.noreply.github.com>
2023-05-14 13:16:45 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class Car(ABC):
+    SERVICE_INTERVAL = None
+
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
 
-    @abstractmethod
+    def service_threshold_date(self):
+        return self.last_service_date.replace(year=self.last_service_date.year + self.SERVICE_INTERVAL)
+
     def needs_service(self):
-        pass
+        if self.service_threshold_date() < datetime.today().date():
+            return True
+        else:
+            return False
