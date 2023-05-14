95581a70f353a53959356e79c456cf92281eadcd
tejasviarora23 <133563625+tejasviarora23@users.noreply.github.com>
2023-05-14 13:14:48 +0530

    Update rorschach.py

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -1,12 +1,29 @@
 from datetime import datetime
+from engine import CapuletEngine, WilloughbyEngine, SternmanEngine
 
-from engine.willoughby_engine import WilloughbyEngine
+
+def engine_class_with_service_threshold(engine_class, years):
+    class NewEngine(engine_class):
+        def needs_service(self):
+            service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + years)
+            if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+                return True
+            else:
+                return False
+    return NewEngine
 
 
-class Rorschach(WilloughbyEngine):
-    def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+class Calliope(engine_class_with_service_threshold(CapuletEngine, 2)):
+    pass
+
+
+class Glissade(engine_class_with_service_threshold(WilloughbyEngine, 2)):
+    pass
+
+
+class Palindrome(engine_class_with_service_threshold(SternmanEngine, 4)):
+    pass
+
+
+class Rorschach(engine_class_with_service_threshold(WilloughbyEngine, 4)):
+    pass
